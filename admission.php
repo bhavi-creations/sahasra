d3b7944702bb47386e5a811fdea500754fef67b0
Ramesh <rameshpilli1428@gmail.com>
2024-03-26 19:13:47 +0530

    changes

--- admission.php
+++ admission.php
@@ -20,14 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'rameshpilli1428@gmail.com'; // Your Gmail email address
-        $mail->Password = 'jjpksiywaevdyyrc'; // Your Gmail password
+        $mail->Username = 'ssvasudeva03136@gmail.com'; // Your Gmail email address
+        $mail->Password = 'wedarbytmwsrezet'; // Your Gmail password
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
         // Recipients
-        $mail->setFrom('rameshpilli1428@gmail.com', 'sahasra'); // Your Gmail email and name
-        $mail->addAddress('rameshpilli1428@gmail.com', 'sahasra'); // Recipient's email and name
+        $mail->setFrom('ssvasudeva03136@gmail.com', 'sahasra'); // Your Gmail email and name
+        $mail->addAddress('ssvasudeva03136@gmail.com', 'sahasra'); // Recipient's email and name
 
         // Content
         $mail->isHTML(true);
